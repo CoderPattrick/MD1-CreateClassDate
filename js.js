82814eee327f2f7bb2e29e29696f0a9a982e28a9
Dat <trandat251111@gmail.com>
2022-01-18 11:31:33 +0700

    update1

--- js.js
+++ js.js
@@ -7,31 +7,67 @@ let MyDate = function(date,month,year){
     this.date=date;
     this.month=month;
     this.year=year;
+    // this.getDate = function (){
+    //     return this.date;
+    // }
+    // this.getMonth = function (){
+    //     return this.month;
+    // }
+    // this.getYear = function (){
+    //     return this.year;
+    // }
 }
-function getDate(x){
-    return x.date;
+MyDate.prototype.getDate = function (){
+    return this.date;
 }
-function getMonth(x){
-    return x.month;
+MyDate.prototype.getMonth = function (){
+    return this.month;
 }
-function getYear(x){
-    return x.year;
+MyDate.prototype.getYear = function (){
+    return this.year;
 }
-let setDate = function (x,y){
-    x.date = y;
+MyDate.prototype.setDay = function (x){
+    this.date = x;
 }
-let setMonth = function (x,y){
-    x.month = y;
+MyDate.prototype.setMonth = function (x){
+    this.month = x;
 }
-let setYear = function (x,y){
-    x.year = y;
+MyDate.prototype.setYear = function (x){
+    this.year = x;
 }
+
+// function getDate(x){
+//     return x.date;
+// }
+// function getMonth(x){
+//     return x.month;
+// }
+// function getYear(x){
+//     return x.year;
+// }
+// let setDate = function (x,y){
+//     x.date = y;
+// }
+// let setMonth = function (x,y){
+//     x.month = y;
+// }
+// let setYear = function (x,y){
+//     x.year = y;
+// }
 let today = new MyDate(14,1,2022);
-console.log(getDate(today));
-console.log(getMonth(today));
-console.log(getYear(today));
-setDate(today,15);
-console.log(getDate(today));
+// console.log(getDate(today));
+// console.log(getMonth(today));
+// console.log(getYear(today));
+// setDate(today,15);
+// console.log(getDate(today));
+console.log(today.getDate());
+console.log(today.getMonth());
+console.log(today.getYear());
+function displayDate(){
+    return today.date+"/"+today.month+"/"+today.year;
+}
+console.log(displayDate())
+
 
 
 
